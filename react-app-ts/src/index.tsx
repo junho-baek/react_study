133e0a6edfdaf0c9b0af893f897c808ddf0830bc
junho-baek <junho6610@yonsei.ac.kr>
2025-01-24 00:47:33 +0900

    3.2

---
+++ react-app-ts/src/index.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+// import ReactDOM from "react-dom";
+import { ThemeProvider } from "styled-components";
+import App from "./App";
+import ReactDOM from "react-dom/client";
+
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement
+);
+
+const darkTheme = {
+  textColor: "whitesmoke",
+  backgroundColor: "#111",
+};
+
+const lightTheme = {
+  textColor: "#111",
+  backgroundColor: "whitesmoke",
+};
+
+root.render(
+  <React.StrictMode>
+    <ThemeProvider theme={darkTheme}>
+      <App />
+    </ThemeProvider>
+  </React.StrictMode>
+);
