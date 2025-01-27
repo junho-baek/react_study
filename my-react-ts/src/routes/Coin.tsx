cacd522327b5a073e8c290fa2fad717806b800ab
junho-baek <junho6610@yonsei.ac.kr>
2025-01-28 05:43:11 +0900

    5.3

--- my-react-ts/src/routes/Coin.tsx
+++ my-react-ts/src/routes/Coin.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useLocation, useParams } from "react-router";
 import styled from "styled-components";
 
@@ -13,27 +13,61 @@ const Loader = styled.span.attrs({
 })``;
 
 const Container = styled.div.attrs({
-  className: "px-5 max-w-[480px] mx-auto",
+  className: "px-5 max-w-[480px] mx-auto h-screen",
 })``;
 
 const Header = styled.header.attrs({
-  className: "h-[15vh] flex justify-center items-center flex-col",
+  className: "h-[20vh] flex justify-center items-center flex-col ",
 })``;
 const Img = styled.img.attrs({
-  className: "w-[35px] h-[35px] mr-[10px]",
+  className: "w-[50px] h-[50px] my-[40px]",
 })``;
 
 function Coin() {
   const [loading, setLoading] = useState(true);
   const { coinId } = useParams(); // 파라미터는 어차피 스트링이다. url 에서 가져오기 때문이지<div className=""></div>
   const { state } = useLocation();
+  const [info, setInfo] = useState();
+  const [price, setPrice] = useState();
+
+  useEffect(() => {
+    (async () => {
+      const infoData = await (
+        await fetch(
+          `https://ohlcv-api.nomadcoders.workers.dev?coinId=${coinId}`
+        )
+      ).json();
+      const priceData = await (
+        await fetch(
+          `https://ohlcv-api.nomadcoders.workers.dev?coinId=${coinId}`
+        )
+      ).json();
+      setInfo(infoData);
+      setPrice(priceData);
+      console.log(infoData, priceData);
+      setLoading(false);
+    })();
+  }, []);
+
   return (
     <Container>
       <Header>
-        <Img src={`https://cryptocurrencyliveprices.com/img/${coinId}.png`} />
-        <Title>{`${state?.name} (${state?.id})`}</Title>
+        <Img
+          className=""
+          src={`https://cryptocurrencyliveprices.com/img/${coinId}.png`}
+        />
+        <Title className="mb-[10px] text-center">{`${
+          state?.name || "Loading..."
+        }`}</Title>
       </Header>
-      {loading ? <Loader>Loading...</Loader> : null}
+      {loading ? (
+        <>
+          <Loader className="text-center mt-[100px] mb-[30px] animate-pulse">
+            loding..
+          </Loader>
+          <Loader className=" animate-spin text-[100px] w-auto">↻</Loader>
+        </>
+      ) : null}
     </Container>
   );
 }
